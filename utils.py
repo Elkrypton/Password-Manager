66d1c74feb44fad84d6cec63202194d753850093
Elkrypton <rochdielmajdoub@gmail.com>
2023-08-27 19:19:45 -0400

    change utils

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from cryptography.fernet import Fernet
 
 class CryptTools():
 
-        def __init__(self, text, key):
+        def __init__(self, text):
                 self.text = str(text)
                 self.key = self.FindKey()
 
@@ -17,7 +17,31 @@ class CryptTools():
                                 content = open(file_, 'r')
                                 key += content
                         else:
-                                print(">> file does not exist")
+                                print("File does not exist, do you want to create it?")
+                                choice = input("Yes/No")
+                                if choice == "yes":
+                                        self.GenerateKey()
+
                 return key
         
-                        
+        
+        def GenerateKey(self):
+                filename = "keys.txt"
+                _key = Fernet.generate_key()
+                key = _key.decode()
+                with open(filename) as f:
+                        f.write(key)
+                
+                f.close()
+
+
+        def Encrypt(self):
+                return Fernet(self.key).Encrypt(self.text)
+        
+        def Decrypt(self):
+                return Fernet(self.key).Decrypt(self.text)
+
+
+if __name__ == "__main__":
+        text = input(">> Please provide a text to encrypt:")
+        key = 
